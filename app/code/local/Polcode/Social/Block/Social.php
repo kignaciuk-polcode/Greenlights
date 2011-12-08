5ae944f585024636b4219f6feccf548c9e55d3f4
Paweł Rybarczyk <rybarczyk.pawel@gmail.com>
2011-12-08 12:48:32 +0100

    Created module Polcode_Social. Social share buttons moved to Social module block.

---
+++ app/code/local/Polcode/Social/Block/Social.php
@@ -0,0 +1,9 @@
+<?php
+
+class Polcode_Social_Block_Social extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        $this->setTemplate('social/social.phtml');
+    }
+}
