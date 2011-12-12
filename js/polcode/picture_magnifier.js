e2739a772706bd2abaa858694320d808523a3eaa
Paweł Rybarczyk <rybarczyk.pawel@gmail.com>
2011-12-12 11:37:48 +0100

    Feature #2395 enlarging photos.

--- js/polcode/picture_magnifier.js
+++ js/polcode/picture_magnifier.js
@@ -1,13 +1,12 @@
-function pictureMagnifier (id) {
+function pictureMagnifier (id, imgSrc) {
 	var placeToInsertPhoto = $$(".product-shop")[0];
 	var foto = $(id);
 	
 	var newFotoContainer = document.createElement("div");
 	newFotoContainer.setAttribute('id', foto.getAttribute('id') + '_container');
-	newFotoContainer.setAttribute('style', 'background:#f00;');
 	
 	var newFoto = document.createElement("img");
-	newFoto.setAttribute('src', foto.getAttribute('src'));
+	newFoto.setAttribute('src', imgSrc);
 	
 	newFotoContainer.appendChild(newFoto);
 	
