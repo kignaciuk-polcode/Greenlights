2707861fc40e2ea717e4d6c2267241531bf98508
Paweł Rybarczyk <rybarczyk.pawel@gmail.com>
2011-12-12 10:34:08 +0100

    Feature #2395 After "on mouse over" event there is created div for enlarged foto and insert on top of description. After "on mouse out" event, div is removed.

--- js/polcode/picture_magnifier.js
+++ js/polcode/picture_magnifier.js
@@ -1,3 +1,27 @@
 function pictureMagnifier (id) {
-//	alert(id);
+	var placeToInsertPhoto = $$(".product-shop")[0];
+	var foto = $(id);
+	
+	var newFotoContainer = document.createElement("div");
+	newFotoContainer.setAttribute('id', foto.getAttribute('id') + '_container');
+	newFotoContainer.setAttribute('style', 'background:#f00;');
+	
+	var newFoto = document.createElement("img");
+	newFoto.setAttribute('src', foto.getAttribute('src'));
+	
+	newFotoContainer.appendChild(newFoto);
+	
+	placeToInsertPhoto.insert({
+		'after' : newFotoContainer
+	});
+	
+	newFotoContainer.absolutize();
+	newFotoContainer.clonePosition(placeToInsertPhoto);
+	
+}
+
+function removePictureMagnifier(id) {
+	var foto = $(id);
+	var newFotoContainer = $(foto.getAttribute('id') + '_container');
+	newFotoContainer.remove();
 }
