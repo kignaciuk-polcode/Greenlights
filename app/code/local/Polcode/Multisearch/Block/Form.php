63808471c7c43727f88d5b272ae9fe271102c1b1
Paweł Rybarczyk <rybarczyk.pawel@gmail.com>
2011-12-15 14:02:27 +0100

    Multisearch Module files.

---
+++ app/code/local/Polcode/Multisearch/Block/Form.php
@@ -0,0 +1,9 @@
+<?php
+
+class Polcode_MultiSearch_Block_Form extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        $this->setTemplate('multisearch/form.mini.phtml');
+    }
+}
---
+++ app/code/local/Polcode/Multisearch/Block/Result.php
@@ -0,0 +1,9 @@
+<?php
+
+class Polcode_MultiSearch_Block_Result extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        $this->setTemplate('multisearch/result.phtml');
+    }
+}
